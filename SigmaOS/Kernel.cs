e2f5da3fdddf9bf9780ade9d9bbdf4c526e877fa
itsha123 <itsha123@proton.me>
2024-05-19 11:14:48 -0500

    Add project files.

---
+++ SigmaOS/Kernel.cs
@@ -0,0 +1,34 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Sys = Cosmos.System;
+using CurrentTime;
+using Cosmos.System.Graphics.Fonts;
+
+namespace SigmaOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        VBECanvas canvas;
+        Time time;
+        Font font;
+        Mode mode;
+        protected override void BeforeRun()
+        {
+            mode = new Mode(800, 600, ColorDepth.ColorDepth32);
+            canvas = new VBECanvas(mode);
+            canvas.Clear(Color.Blue);
+            canvas.Display();
+        }
+
+        protected override void Run()
+        {
+            canvas.Clear(Color.Blue);
+            canvas.DrawString("Welcome to SigmaOS!", PCScreenFont.Default, Color.White, 348, 274);
+            canvas.DrawString(time.TimeString(), PCScreenFont.Default, Color.White, 348, 292);
+            canvas.Display();
+        }
+    }
+}
---
+++ SigmaOS/Time.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CurrentTime
+{
+    public class Time
+    {
+        public long UnixTime()
+        {
+            // Get current time in UTC
+            DateTimeOffset nowUtc = DateTimeOffset.UtcNow;
+
+            // Convert to Unix timestamp (milliseconds)
+            long unixTimeInMilliseconds = nowUtc.ToUnixTimeMilliseconds();
+            return unixTimeInMilliseconds;
+        }
+        public string TimeString()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
